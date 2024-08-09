537e0dcf58cb5663d0e306c8db755a1df237c52a
Rupanzil Prince <rupanzilmamunprince@gmail.com>
2024-08-10 00:08:56 +0530

    created server.js

--- learning express/server.js
+++ learning express/server.js
@@ -1,2 +1,21 @@
-console.log('Hello')
-e
+const express = require('express')
+const app = express()
+console.log(app)
+const PORT = 3000
+
+// simple route
+app.get('/', (req, res) => {
+  res.send('Hello from the server')
+  console.log('success')
+})
+
+// create another route for login page
+app.get('/new-user', (req, res) => {
+  res.send('New user login page')
+  console.log('login page accessed')
+})
+
+// listening on port
+app.listen(PORT, () => {
+  console.log('Listening on port 3000')
+})
